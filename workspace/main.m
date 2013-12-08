a08ca49fe8244f0266ed0ef4dd54419b79f7010e
Jian Jiang <jjlzyz000@gmail.com>
2013-12-08 16:14:08 -0500

    Get blockList in main. Get pre-computed color distance.

--- workspace/main.m
+++ workspace/main.m
@@ -9,9 +9,7 @@ originTarget1 = double(imread('target1.png'));	% Target Images
 originTarget2 = double(imread('target2.png'));
 originTarget3 = double(imread('target3.png'));
 
-% Turn them into gray scale Images
-templateImage = rgb2gray(originTemplateImage);
-target1 = rgb2gray(originTarget1);
-target2 = rgb2gray(originTarget2);
-target3 = rgb2gray(originTarget3);
+blockList = blockSplit(originTemplateImage);
+dis = color_dis();
+
 
